7aabb84a7c046652264a97619a1ad903599838b2
fabiomarcell <fabio.marcell@outlook.com>
2016-12-02 15:09:32 -0200

    Method Put para atualizar documentos, e Delete para excluir documentos, agora funcionam corretamente

--- Controller/routes.js
+++ Controller/routes.js
@@ -6,13 +6,51 @@ var Tarefa = require("../Model/TarefaModel.js");
 //app.method('route', callback(req,res){})
 module.exports = function(app){
   app.get('/', function (req, res) {
-  	res.send(JSON.stringify("Acessando a Index do Projeto <br>" +
-  						"Rotas: <br>" +
-  						"/insert <br>" +
-  						"/update <br>" +
-  						"/delete <br>" +
-  						"/select <br>" +
-  						"/select-one/{id}"));
+  	res.json({
+              "":"Bem Vindo a Index do Projeto!",
+              "Rotas": [{
+                "Method": "Post",
+                "Rota": "/insert",
+                "Header": "Content-type:application/json",
+                "Tipo de Parametro": "Json",
+                "Parametros": {
+                  "titulo": "string",
+                  "descricao": "string"
+                }
+              },
+              {
+                "Method": "Put",
+                "Rota": "/update",
+                "Header": "Content-type:application/json",
+                "Tipo de Parametro": "Json",
+                "Parametros": {
+                  "id": "String",
+                  "titulo": "string",
+                  "descricao": "string"
+                }
+              },
+              {
+                "Method": "Delete",
+                "Rota": "/delete",
+                "Header": "Content-type:application/json",
+                "Tipo de Parametro": "Json",
+                "Parametros": {
+                  "id": "String"
+                }
+              },
+              {
+                "Method": "Get",
+                "Rota": "/select",
+                "Header": "Content-type:application/json",
+                "Tipo de Parametro": "Json"
+              },
+              {
+                "Method": "Get",
+                "Rota": "/select-one/{id:string}",
+                "Header": "Content-type:application/json",
+                "Tipo de Parametro": "Json"
+              }]
+            });
   	console.log("Index.");
   });
 
@@ -33,6 +71,37 @@ module.exports = function(app){
   			res.json({"status": true, "message": "Cadastro efetuado"});
   		}
   	});
+  });
+
+  app.put('/update', function (req, res) {
+  	console.log("Update.");
+    var query = {_id: req.body.id};
+    var updateQuery = {"titulo" : req.body.titulo, "descricao": req.body.descricao};
+    Tarefa.update(query, {"$set": updateQuery}, function(err){
+      if (err){
+  		    res.json({"status": false, "message": err});
+  		}
+  		else{
+  			res.json({"status": true, "message": "Alteração efetuada"});
+  		}
+    });
+    //res.json({ "method":"Put method", "body": req.body});
+
+  });
+
+  app.delete('/delete', function (req, res) {
+  	console.log("Delete.");
+    var query = {_id: req.body.id};
+    //var updateQuery = {"titulo" : req.body.titulo, "descricao": req.body.descricao};
+    Tarefa.remove(query, function(err){
+      if (err){
+  		    res.json({"status": false, "message": err});
+  		}
+  		else{
+  			res.json({"status": true, "message": "Exclusão efetuada"});
+  		}
+    });
+
   });
 
   app.get('/select', function (req, res) {
@@ -60,4 +129,5 @@ module.exports = function(app){
   	});
 
   });
+
 };
